52f83f1eaf8f75e0df1461d57b01151f384bfa7d
wchau <wchau8899@gmail.com>
2017-11-08 00:05:21 -0700

    undo last commit

--- app/models/index.js
+++ app/models/index.js
@@ -8,8 +8,8 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.production.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.production.use_env_variable]);
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
