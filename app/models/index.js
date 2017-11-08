5de006b6240413da7f4852bf4e3446b4cc793fc9
wchau <wchau8899@gmail.com>
2017-11-08 00:23:02 -0700

    use process.env.JAWSDB in index.js

--- app/models/index.js
+++ app/models/index.js
@@ -9,7 +9,7 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (process.env.JAWSDB_URL) {
-  var sequelize = new Sequelize(process.env[process.env.JAWSDB_URL]);
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
