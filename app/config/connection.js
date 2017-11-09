d1c9f39ca1175e388272636e3337c1e9abe41700
meglambda <meganlandahl@gmail.com>
2017-11-08 20:50:10 -0700

    Fixed dollar/day sign

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,6 +1,6 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("sherpadb", "root", null,{
+var sequelize = new Sequelize("sherpadb", null, null,{
 	host: "localhost",
 	dialect: "mysql",
 	pool: {
